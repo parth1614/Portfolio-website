cccd2ce0b6e6a38ed6e4fc57a430aecef0e00f48
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-14 14:24:34 +0530

    Add files via upload

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from "../Title";
+
+function About(){
+    return(
+        <div className="AboutPage">
+            <Title title={'About Me'} span={'About Me'} />
+
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Portfolio(){
+    return(
+        <div className="Portfolio">
+            <h1>Portfolio</h1>
+
+        </div>
+    );
+}
+
+export default Portfolio;
