a8203f5a378e392f7195794feac2f946261138d9
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-16 12:37:35 +0530

    Add files via upload

---
+++ build/precache-manifest.0822de1e3dd9e8604f5b89b28b6cf6a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d4a90fd248afa961f1",
+    "url": "/static/css/main.7ea17add.chunk.css"
+  },
+  {
+    "revision": "99d4a90fd248afa961f1",
+    "url": "/static/js/main.fefad258.chunk.js"
+  },
+  {
+    "revision": "a0574b161693035f2e63",
+    "url": "/static/js/runtime~main.a94f6b62.js"
+  },
+  {
+    "revision": "c5f65a46f8ea83a4e1e3",
+    "url": "/static/js/2.721cb066.chunk.js"
+  },
+  {
+    "revision": "447f48571f66e7807be5",
+    "url": "/static/js/3.d69099ea.chunk.js"
+  },
+  {
+    "revision": "0a6dd2e7e1a5bf65570adcebc6fb00c6",
+    "url": "/static/media/profile.0a6dd2e7.svg"
+  },
+  {
+    "revision": "2e068ac2895add6fd381e023ce111669",
+    "url": "/static/media/profile.2e068ac2.jpg"
+  },
+  {
+    "revision": "bca243ce67ac4832cda78caca8c24d47",
+    "url": "/static/media/design.bca243ce.svg"
+  },
+  {
+    "revision": "55653faf478d687b6b3d0a744ba0b303",
+    "url": "/static/media/dots.55653faf.svg"
+  },
+  {
+    "revision": "c0641a320f57eb9f7316fa3c9a266664",
+    "url": "/index.html"
+  }
+];
